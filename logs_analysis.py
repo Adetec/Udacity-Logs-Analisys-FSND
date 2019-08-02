3bce2cd3294feb990887954c80c080a64d174553
Adel Lassag <adetech.aadl@gmail.com>
2019-08-03 00:04:16 +0100

    feat: Store questions and their empty queries in a list

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,3 +1,20 @@
 #!/usr/bin/env python3
 
 import psycopg2
+
+DBNAME = 'news'
+
+queries = [
+    {
+        'question': 'What are the most popular three articles of all time',
+        'query': '',
+    },
+    {
+        'question': 'Who are the most popular article authors of all time',
+        'query': '',
+    },
+    {
+        'question': 'On which days did more than 1% of requests lead to errors',
+        'query': '',
+    }
+]
